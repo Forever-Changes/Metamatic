5b22afe817d8cbd275771f166db61232eb946e4a
Arron Ware <70115287+Forever-Changes@users.noreply.github.com>
2021-12-18 11:55:40 +0000

    Add files via upload

---
+++ Portfolio-main/assets/inc/header.php
@@ -0,0 +1,12 @@
+<header>
+    
+    
+    <link rel="icon" href="assets/icons/worldofcopland1/oldtv.ico">
+
+    <title>Métamatic</title>
+
+    <link href="stylesheet.css" rel="stylesheet" type="text/css">
+
+    <link href="../stylesheet.css" rel="stylesheet" type="text/css">
+
+</header>
