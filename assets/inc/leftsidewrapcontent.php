f0209c58cc1ca9e8bc66a46cf7dbaca87f8b8b9f
Arron Ware <70115287+Forever-Changes@users.noreply.github.com>
2021-12-30 17:32:09 +0000

    Add files via upload

---
+++ assets/inc/leftsidewrapcontent.php
@@ -0,0 +1,17 @@
+<div id="leftsidewrap">
+                    <a class="menu_link" href="default.php"><div id="logobox"></div></a>
+                
+                    <div id="menubox">
+                        <div id="menuitems"><a href="default.php">Home</a></div>
+                        <div id="menuitems"><a href="polaroids.php">Photography</a></div>
+                        <div id="menuitems"><a href="music.php">Music</a></div>
+                        <div id="menuitems"><a href="collections.php">Collections</a></div>
+                        <div id="menuitems"><a href="trivia.php">Trivia</a></div>
+                        <div id="menuitems"><a href="resources.php">Resources</a></div>
+                        <div id="menuitems"><a href="links.php">Links</a></div>
+                        <div id="menuitems"><a href="contact.php">Contact</a></div>
+                            
+                    </div>
+                    
+                    
+                </div>
---
+++ assets/inc/modals/teacards/brookebond/historyofmotorcar.php
@@ -0,0 +1,11 @@
+<div class="modal">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h2 class="close-modal" onclick="document.getElementById('modal').style.display='none'">&times;</h2>
+            <h1>Title</h1>
+        </div>
+        <div class="modal-body">
+            
+        </div>
+    </div>
+</div>
