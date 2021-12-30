14c3216a87ba23ca8f937d9561b8cf98166ddee6
Arron Ware <70115287+Forever-Changes@users.noreply.github.com>
2021-12-30 17:22:58 +0000

    Add files via upload

---
+++ default.php
@@ -0,0 +1,52 @@
+<html>
+
+    <?php include 'assets/inc/header.php'; ?>
+
+
+
+    <body>
+
+        <div id="wrap">
+
+            <div id="header"></div>
+                
+                <?php include 'assets/inc/leftsidewrapcontent.php'; ?>
+
+                <div id="rightsidewrap">
+                    <div class="title">Home</title></div>
+                    <img class="imgbord" id="mehomeimg"
+                    src="assets/img/me1.jpg" alt="Arron Ware, Me, Site management n all that jazz">
+                    
+                    <div id="textalongsideimage">
+
+                        <p class="imgcredit">Arron Ware, site owner</p>
+                        <h4>- - - - - - - - - - - - - - - - - - - </h4>
+                        <p>Hello, welcome to my site!
+                        <br><br>
+                            Bit of everything on here:   <br>
+                            Stuff I collect (Tea/Cigarette cards, CDs/Tapes/Records, VHS tapes/DVDs etc), 
+                            I do a decent bit of photography so I've popped a bit of that on here. 
+                            <br><br>
+                            Just anything that I feel like sticking on here I suppose. Hope you enjoying having a look round =|:)</p>
+
+                        
+                    </div>
+
+
+
+                    
+                    <div class="line"></div>
+                    <?php include 'assets/inc/footer.php'; ?>
+                </div>
+
+                
+
+            </div>   
+            
+        </div>
+    
+    </body>
+
+    
+
+</html>
---
+++ polaroids.php
@@ -0,0 +1,385 @@
+<html>
+
+
+<?php include 'assets/inc/header.php'; ?>
+
+
+
+
+<body>
+
+    <!------------------------------- Modals --------------------------->
+
+    <div id="cooks2021modal" class="bgmodal" onclick="document.getElementById('cooks2021modal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('cooks2021modal').style.display='none'">&times;</h2>
+                <h1>2020</h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/cooks2021.jpg" alt="Cookie on da floor" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <div id="skyseamodal" class="bgmodal" onclick="document.getElementById('skyseamodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('skyseamodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/skysea.jpg" alt="" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+    
+
+
+
+                
+                
+                
+
+
+    <div id="vanlakemodal" class="bgmodal" onclick="document.getElementById('vanlakemodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('vanlakemodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/vanlake.jpg" alt="" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+
+
+                
+                
+                
+                
+                
+    <div id="zoobirdmodal" class="bgmodal" onclick="document.getElementById('zoobirdmodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('zoobirdmodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/lonelyzoobird.jpg" alt="" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+
+
+                
+                
+                
+                
+                
+                
+    <div id="scarbdocksmodal" class="bgmodal" onclick="document.getElementById('scarbdocksmodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('scarbdocksmodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/scarbdocks.jpg" alt="" width="100%" class="portrait"> 
+            </div>
+        </div>
+    </div>
+
+                
+                
+                
+                
+                
+    <div id="scarbhotelmodal" class="bgmodal" onclick="document.getElementById('scarbhotelmodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('scarbhotelmodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/scarboroughhotel.jpg" alt="Scarborough Hotel" width="100%" class="portrait" > 
+            </div>
+        </div>
+    </div>
+
+                
+                
+                
+                
+                
+                
+    <div id="fileybeachmodal" class="bgmodal" onclick="document.getElementById('fileybeachmodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('fileybeachmodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/fileybeach.JPG" alt="" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+
+                
+                
+                
+                
+                
+                
+    <div id="fileylighthousemodal" class="bgmodal" onclick="document.getElementById('fileylighthousemodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('fileylighthousemodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/fileylighthouse.jpg" alt="" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+
+    <div id="lighthousemodal" class="bgmodal" onclick="document.getElementById('lighthousemodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('lighthousemodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/lighthouse.JPG" alt="" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+
+    <div id="nycparktreemodal" class="bgmodal" onclick="document.getElementById('nycparktreemodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('nycparktreemodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/nycparktree.jpg" alt="" width="100%" class="portrait"> 
+            </div>
+        </div>
+    </div>
+
+    <div id="gothicceilingmodal" class="bgmodal" onclick="document.getElementById('gothicceilingmodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('gothicceilingmodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/bedroomceilinggoth.jpg" alt="" width="100%" class="portrait"> 
+            </div>
+        </div>
+    </div>
+
+    <div id="nycskylinestreetmodal" class="bgmodal" onclick="document.getElementById('nycskylinestreetmodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('nycskylinestreetmodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/nycskylinestreetov.jpg" alt="" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+
+    <div id="santafestmodal" class="bgmodal" onclick="document.getElementById('santafestmodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('santafestmodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/santafest2019.jpg" alt="" width="100%" class="portrait"> 
+            </div>
+        </div>
+    </div>
+
+
+    <div id="evilempiremodal" class="bgmodal" onclick="document.getElementById('evilempiremodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('evilempiremodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/evilempire.jpg" alt="" width="100%" class="portrait"> 
+            </div>
+        </div>
+    </div>
+
+    <div id="gothictreetrainmodal" class="bgmodal" onclick="document.getElementById('gothictreetrainmodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('gothictreetrainmodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/gothingtreetrain.jpg" alt="" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+
+    <div id="cheeseboardmodal" class="bgmodal" onclick="document.getElementById('cheeseboardmodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('cheeseboardmodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/cheeseboard.jpg" alt="" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+
+
+    <div id="lakedismodal" class="bgmodal" onclick="document.getElementById('lakedismodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('lakedismodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/lakedis.jpg" alt="" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+
+    <div id="treefallmodal" class="bgmodal" onclick="document.getElementById('treefallmodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('treefallmodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/treecoverwaterfall.jpg" alt="" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+
+
+    <div id="fieldmodal" class="bgmodal" onclick="document.getElementById('fieldmodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('fieldmodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/fieldinnit.jpg" alt="" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+
+
+    <div id="benidormmodal" class="bgmodal" onclick="document.getElementById('benidormmodal').style.display='none'">
+        <div class="modalcontent">
+            <div class="modalheader">
+                <h2 class="closemodal" onclick="document.getElementById('benidormmodal').style.display='none'">&times;</h2>
+                <h1></h1>
+            </div>
+            <div class="modalbody">
+                <img src="assets/img/photography/benidorm.jpg" alt="" width="100%" class="landscape"> 
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+    
+    <div id="wrap">
+
+        
+
+        <div id="header"></div>
+            
+            <?php include 'assets/inc/leftsidewrapcontent.php'; ?>
+
+            <div id="rightsidewrap">
+                <div class="title">Photography</title></div>
+                
+                <img class="imgbord" id="mehomeimgphotography"
+                    src="assets/img/me1.jpg">
+
+                <div id="textalongsideimage">
+
+                    <p class="imgcreditp">Arron Ware</p>
+                    <p class="imgcreditp">------------</p>
+                    <h2>Gear</h2>
+
+                    
+                    <p class="gear">SLR - Canon EOS 500 (1993)</p>
+                    <p>--</p>
+                    <p class="gear">DSLR - Nikon D90 (2008)</p>
+                    <p>--</p>
+                    <p class="gear">Smartphone - Oneplus 6 (2018)</p>
+                
+                </div>
+                <h2>---------------------------</h2>
+                
+
+                <div class="imgbord" id="cooks2021" onclick="document.getElementById('cooks2021modal').style.display='flex'"></div>
+                <div class="imgbord" id="skysea" onclick="document.getElementById('skyseamodal').style.display='flex'"></div>
+                <div class="imgbord" id="vanlake" onclick="document.getElementById('vanlakemodal').style.display='flex'"></div>
+                <div class="imgbord" id="zoobird" onclick="document.getElementById('zoobirdmodal').style.display='flex'"></div>
+                <div class="imgbord" id="scarbdocks" onclick="document.getElementById('scarbdocksmodal').style.display='flex'"></div>
+                <div class="imgbord" id="scarbhotel" onclick="document.getElementById('scarbhotelmodal').style.display='flex'"></div>
+                <div class="imgbord" id="fileybeach" onclick="document.getElementById('fileybeachmodal').style.display='flex'"></div>
+                <div class="imgbord" id="fileylighthouse" onclick="document.getElementById('fileylighthousemodal').style.display='flex'"></div>
+                <div class="imgbord" id="lighthouse" onclick="document.getElementById('lighthousemodal').style.display='flex'"></div>
+                <div class="imgbord" id="nycparktree"  onclick="document.getElementById('nycparktreemodal').style.display='flex'"></div>
+                <div class="imgbord" id="gothicceiling" onclick="document.getElementById('gothicceilingmodal').style.display='flex'"></div>
+                <div class="imgbord" id="nycskylinestreet" onclick="document.getElementById('nycskylinestreetmodal').style.display='flex'"></div>
+                <div class="imgbord" id="santafest" onclick="document.getElementById('santafestmodal').style.display='flex'"></div>
+                <div class="imgbord" id="evilempire" onclick="document.getElementById('evilempiremodal').style.display='flex'"></div>
+                <div class="imgbord" id="gothtreetrain" onclick="document.getElementById('gothictreetrainmodal').style.display='flex'"></div>
+                <div class="imgbord" id="cheeseboard" onclick="document.getElementById('cheeseboardmodal').style.display='flex'"></div>
+                <div class="imgbord" id="lakedis" onclick="document.getElementById('lakedismodal').style.display='flex'"></div>
+                <div class="imgbord" id="treefall" onclick="document.getElementById('treefallmodal').style.display='flex'"></div>
+                <div class="imgbord" id="field" onclick="document.getElementById('fieldmodal').style.display='flex'"></div>
+                <div class="imgbord" id="benidorm" onclick="document.getElementById('benidormmodal').style.display='flex'"></div>
+
+                
+                
+                
+
+                
+
+                
+
+
+               
+                <div>
+                <div class="line"></div>
+                
+                <?php include 'assets/inc/footer.php'; ?>
+            </div>
+            
+
+            </div>
+
+            
+            
+        <div>  
+        
+    </div>
+
+</body>
+
+
+</html>
